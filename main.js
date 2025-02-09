e51096e2c8bbf671543a42ec06964a561cff2ee0
ketupatkucing <gammertag31@gmail.com>
2025-02-10 00:26:17 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 function generatePDF() {
   setTimeout(() => {
     const element = document.getElementById('myForm');
@@ -13,9 +11,38 @@ function generatePDF() {
       })
       .from(element)
       .save();
-  
-  
+
+
   }, 100);
+
+
+}
+
+const d = document.getElementById('name')
+const tgl = document.getElementById('tanggal')
+
+function cek() {
+  const c = d.textContent.trim()
+  if (c.length > 40) {
+    //alert('Oops kepanjangan')
+    d.innerText = d.innerText.substring(0, 3);
+    d.innerText = "Nama Peserta"
+  }
+  if (d.offsetHeight > 59){
+    //alert('oops ketinggian')
+    d.innerText = d.innerText.substring(0, 1);
+    d.innerText = "Nama Peserta"
+  }
   
-    
+}
+function cektgl(){
+  const t = tgl.textContent.trim()
+  if (t.length > 20) {
+    tgl.innerText = tgl.innerText.substring(0, 10);
+    tgl.innerText = "dd/mm/yy"
+  }
+  if (tgl.offsetHeight > 30) {
+    tgl.innerText = tgl.innerText.substring(0, 10);
+    tgl.innerText = "dd/mm/yy"
+  }
 }
